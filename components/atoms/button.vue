a7b997587c09cab250dc93f054672c0dbaad533a
Diego Lamarão <diego.lamarao92@gmail.com>
2019-08-31 16:49:20 -0300

    created button component

---
+++ components/atoms/button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="app-button" @click="" v-text="text"></button>
+</template>
+
+<script>
+export default {
+  name: 'app-button',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style lang="scss">
+  .app-button {
+
+  }
+</style>
