dfdca4f142ffa90fdee603d465ed1e0bed110d8c
Diego Lamarão <diego.lamarao92@gmail.com>
2019-08-31 21:35:51 -0300

    improved button component

--- components/atoms/button.vue
+++ components/atoms/button.vue
@@ -1,18 +1,59 @@
 <template>
-  <button class="app-button" @click="" v-text="text"></button>
+  <button
+    class="app-button"
+    :class="buttonClasses"
+    @click="$emit('click', $event)"
+    v-text="text"
+  ></button>
 </template>
 
 <script>
 export default {
   name: 'app-button',
   props: {
-    text: String
-  }
-}
+    text: String,
+    color: {
+      type: String,
+      default: 'blue'
+    },
+    fullWidth: Boolean
+  },
+  computed: {
+    buttonClasses () {
+      const classes = [];
+      if (this.color) {
+        classes.push(`bg-${this.color}`);
+      }
+      if (this.fullWidth) {
+        classes.push('full-width');
+      }
+      return classes;
+    }
+  },
+};
 </script>
 
 <style lang="scss">
   .app-button {
-
+    min-height: 30px;
+    padding: 5px 20px;
+    border: none;
+    border-radius: 3px;
+    font-size: 20px;
+    font-weight: bold;
+    &.full-width {
+      width: 100%;
+    }
+    &:hover {
+      opacity: 0.9;
+      cursor: pointer;
+    }
+    &:active {
+      transform: scale(0.95);
+    }
+    &:focus {
+      outline: none;
+      border: none;
+    }
   }
 </style>
